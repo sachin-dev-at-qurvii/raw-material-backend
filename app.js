c9b0c8dfa3d86c288759ab4c367d8b1c40455476
sachin-dev-at-qurvii <komal@qurvii.com>
2025-09-26 10:31:39 +0530

    cors origin updated

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5000;
 // global middlewares 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin:"*",
   credentials: true
 }
 ));
@@ -45,5 +46,6 @@ connectDB().then(() => {
 })
 
 app.use(globalErrorHandler)
+
 
 
