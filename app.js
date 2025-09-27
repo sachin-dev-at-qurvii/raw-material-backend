ef1c88a6a4ade6b91a5106461e5d6ef0efc17e8d
sachin-dev-at-qurvii <komal@qurvii.com>
2025-09-27 14:32:20 +0530

    cors allow from all resoures

--- app.js
+++ app.js
@@ -16,13 +16,15 @@ const PORT = process.env.PORT || 5000;
 
 // global middlewares 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({
-  origin: "http://localhost:5173" || "*",
-  credentials: true
-}
-));
+// app.use(cors({
+//   // origin: "http://localhost:5173" || "*",
+//   origin: "http://localhost:5173" || "*",
+//   credentials: true
+// }
+// ));
 
-// app.use(cors());
+
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
@@ -48,5 +50,6 @@ connectDB().then(() => {
 })
 
 app.use(globalErrorHandler)
+
 
 
