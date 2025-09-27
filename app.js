c11ba60d57e075e6f9f4e3a456f016e48b7746e1
sachin-dev-at-qurvii <komal@qurvii.com>
2025-09-27 11:56:35 +0530

    order notifier controller and route added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const styleRoutes = require("./routes/styleRoute");
 const userRoutes = require("./routes/userRoute");
 const relationShipRoutes = require("./routes/meterAndkgRelationShip.routes.js");
 const fabricAverageRoutes = require("./routes/fabricAvg.routes.js")
+const orderNotifierRoute = require("./routes/orderNotifier.routes.js");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -16,14 +17,12 @@ const PORT = process.env.PORT || 5000;
 // global middlewares 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  // origin: "http://localhost:5173",
-  origin:"*",
+  origin: "http://localhost:5173" || "*",
   credentials: true
 }
 ));
 
 // app.use(cors());
-
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
@@ -37,6 +36,9 @@ app.use("/api/v1/style-details", styleRoutes);
 app.use("/api/v1/relation", relationShipRoutes);
 app.use("/api/v1/average", fabricAverageRoutes);
 
+// order notifier mail 
+app.use("/api/v1/order", orderNotifierRoute);
+
 
 // start server 
 connectDB().then(() => {
@@ -46,6 +48,5 @@ connectDB().then(() => {
 })
 
 app.use(globalErrorHandler)
-
 
 
---
+++ routes/orderNotifier.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const sendNotificationToEmail = require("../controllers/orderNorifier.controller.js");
+const router = express();
+
+router.route("/send").post(sendNotificationToEmail);
+
+module.exports = router;
