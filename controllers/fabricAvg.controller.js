9650bd858060d7071e3573dcbd2b49d7e32b2033
sachin-dev-at-qurvii <komal@qurvii.com>
2025-10-17 15:44:03 +0530

    style_number field added

--- controllers/fabricAvg.controller.js
+++ controllers/fabricAvg.controller.js
@@ -3,6 +3,50 @@ const ApiError = require("../utils/ApiError.js");
 const ApiResponse = require("../utils/ApiResponse.js");
 
 
+// const upsertFabriPatterns = async (req, res, next) => {
+//     try {
+//         const payload = req.body;
+//         if (!Array.isArray(payload) || payload.length === 0) {
+//             return next(new ApiError(400, "Payload must be non-empty array"));
+//         }
+//         let addedCount = 0;
+//         let updatedCount = 0;
+//         let results = [];
+
+//         for (const item of payload) {
+//             const { patternNumber, styleImage, fabrics, accessories } = item;
+//             if (!patternNumber) {
+//                 continue;
+//             }
+//             const existing = await FabricAvg.findOne({ patternNumber });
+//             if (existing) {
+//                 existing.styleImage = styleImage || existing.styleImage;
+//                 existing.fabrics = fabrics || existing.fabrics;
+//                 existing.accessories = accessories || existing.accessories;
+//                 await existing.save();
+//                 updatedCount++;
+//                 results.push({ patternNumber, status: "updated" });
+//             } else {
+//                 const newPattern = await FabricAvg.create({
+//                     patternNumber,
+//                     styleImage,
+//                     fabrics,
+//                     accessories
+//                 });
+//                 addedCount++;
+//                 results.push({ patternNumber, status: "added" });
+//             }
+//         }
+
+//         return res.status(200).json(new ApiResponse(200, "Fabric avg added/updated successfully.", {
+//             addedCount,
+//             updatedCount,
+//             results,
+//         }))
+//     } catch (error) {
+//         next(error);
+//     }
+// }
 const upsertFabriPatterns = async (req, res, next) => {
     try {
         const payload = req.body;
@@ -14,13 +58,14 @@ const upsertFabriPatterns = async (req, res, next) => {
         let results = [];
 
         for (const item of payload) {
-            const { patternNumber, styleImage, fabrics, accessories } = item;
-            if (!patternNumber) {
+            const { patternNumber, styleImage, fabrics, accessories, style_number } = item;
+            if (!style_number || !patternNumber) {
                 continue;
             }
-            const existing = await FabricAvg.findOne({ patternNumber });
+            const existing = await FabricAvg.findOne({ style_number });
             if (existing) {
                 existing.styleImage = styleImage || existing.styleImage;
+                existing.patternNumber = patternNumber || existing.patternNumber;
                 existing.fabrics = fabrics || existing.fabrics;
                 existing.accessories = accessories || existing.accessories;
                 await existing.save();
@@ -28,6 +73,7 @@ const upsertFabriPatterns = async (req, res, next) => {
                 results.push({ patternNumber, status: "updated" });
             } else {
                 const newPattern = await FabricAvg.create({
+                    style_number,
                     patternNumber,
                     styleImage,
                     fabrics,
@@ -48,6 +94,7 @@ const upsertFabriPatterns = async (req, res, next) => {
     }
 }
 
+
 const getFabricPatterns = async (req, res, next) => {
     try {
         const fabrics = await FabricAvg.find();
@@ -59,5 +106,6 @@ const getFabricPatterns = async (req, res, next) => {
         next(error);
     }
 }
+
 
 module.exports = { getFabricPatterns, upsertFabriPatterns }
