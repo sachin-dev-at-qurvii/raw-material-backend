9388c63558f1057ffa29c08cda940fffa5286623
sachin-dev-at-qurvii <komal@qurvii.com>
2025-10-17 14:29:32 +0530

    bulk update stock added

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const { getStock, createStock, updateStock, deleteStock } = require("../controllers/stock.controller");
-const { authenticateJWT, authorizeRoles } = require("../middlewares/auth.middlewares");
+const { getStock, createStock, updateStock, deleteStock, updateMultipleStocks } = require("../controllers/stock.controller");
 const router = express.Router();
 
 
 router.route("/").get(getStock);
-// router.route("/create").post(authenticateJWT, authorizeRoles('admin'), createStock);
-// router.route("/:id").put(authenticateJWT, authorizeRoles("admin"), updateStock);
-// router.route("/:id").delete(authenticateJWT, authorizeRoles("admin"), deleteStock);
 
 
 router.route("/create").post(createStock);
+router.route("/bulk-update").put(updateMultipleStocks);
 router.route("/:id").put(updateStock);
 router.route("/:id").delete(deleteStock);
 
